d34d712bf6eabb2976f24fc3b893ca627a9ac8b4
Luis Lourenço <lhal07@gmail.com>
2021-10-26 22:08:57 -0300

    Open door when player have two keys

---
+++ Assets/Scripts/ActivationDoor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivationDoor : MonoBehaviour
+{
+    private Animator m_Anim;
+    protected AlienInventory m_Alien;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Anim = this.transform.parent.GetComponent<Animator>();
+        m_Alien = (AlienInventory)FindObjectOfType(typeof(AlienInventory));
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if ((collider.gameObject.tag == "Player") && (m_Alien.GetKeys() >= 2) ) {
+            m_Anim.SetTrigger("OpenDoor");
+        }
+    }
+}
